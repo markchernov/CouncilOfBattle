220dafa58767b769070386d2c034fa45adfd22af
markchernov <mark.chernov@gmail.com>
2016-02-16 11:54:26 -0700

    Test

--- src/data/HelperDAO.java
+++ src/data/HelperDAO.java
@@ -1,20 +1,8 @@
 package data;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
