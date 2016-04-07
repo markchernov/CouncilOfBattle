251637f03a68bab17e3b165590aab63d074a6bae
aaron.torres <saarontorres@icloud.com>
2016-04-07 14:57:42 -0600

    cleaned up soe lines in the HelperDAO. No errors, compiles clean.

--- src/data/HelperDAO.java
+++ src/data/HelperDAO.java
@@ -15,7 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class HelperDAO {
 
 	@PersistenceContext
-
 	private EntityManager em;
 
 	/*---------------------- USER METHODS -----------------------*/
